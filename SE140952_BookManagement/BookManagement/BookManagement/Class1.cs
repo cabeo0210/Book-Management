62e11e22d42a8303bbd680a2f3aa3f7eb8e21e7d
cabeo0210 <cabeo0210@gmail.com>
2021-06-29 21:52:19 +0700

    Upload

---
+++ SE140952_BookManagement/BookManagement/BookManagement/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagement
+{
+
+    public class BookLib
+    {
+        class Book
+        {
+            public int bookId;
+            public string bookName;
+            public int bookPrice;
+            public int bookCount;
+            public int x;
+        }
+        static List<Book> bookList = new List<Book>();
+        static Book book = new Book();
+        public void GetBook()
+        {
+            String price;
+            Book book = new Book();
+            Console.WriteLine("\nBook Id:{0}", book.bookId = bookList.Count + 1);
+            Console.Write("Book Name:");
+            book.bookName = Console.ReadLine();           
+            do
+            {
+                Console.Write("Book Price:");
+                price = Console.ReadLine();
+            } while (int.TryParse(price, out book.bookPrice) == false); 
+            Console.Write("Number of Books:");
+            book.x = book.bookCount = int.Parse(Console.ReadLine());
+            bookList.Add(book);
+        }
+
+        public void RemoveBook()
+        {
+            Book book = new Book();
+            Console.Write("Enter Book id to be deleted : ");
+
+            int Del = int.Parse(Console.ReadLine());
+
+            if (bookList.Exists(x => x.bookId == Del))
+            {
+                bookList.RemoveAt(Del - 1);
+                Console.WriteLine("Book id - {0} has been deleted", Del);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Book id");
+            }
+
+            bookList.Add(book);
+        }
+
+
+        public void SearchBook()
+        {
+            Book book = new Book();
+            Console.Write("Search by BOOK id :");
+            int find = int.Parse(Console.ReadLine());
+
+            if (bookList.Exists(x => x.bookId == find))
+            {
+                foreach (Book searchId in bookList)
+                {
+                    if (searchId.bookId == find)
+                    {
+                        Console.WriteLine("| ++ Book id ++ |" + " ++ Book name ++ |" 
+                            + " ++ Book price ++ |" + " ++ Book Count ++ |");
+                        Console.WriteLine(("\t" + searchId.bookId + "   ")
+                            + ("\t\t" + searchId.bookName + "  ") +
+                            ("\t\t   " + searchId.bookPrice + " ") + ("\t\t\t" + searchId.bookCount + " "));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Book id {0} not found", find);
+            }
+        }
+        public void Show()
+        {
+           
+
+                foreach (var book in bookList)
+                {
+                    Console.WriteLine("| ++ Book id ++ |" + " ++ Book name ++ |" + " ++ Book price ++ |" + " ++ Book Count ++ |");
+                    Console.WriteLine(("\t" + book.bookId + "   ") + ("\t\t" + book.bookName + "  ")
+                        + ("\t\t   " + book.bookPrice + " ") + ("\t\t\t" + book.bookCount + " "));
+                }
+
+            
+            
+            
+
+        }
+    }
+}
+
---
+++ SE140952_BookManagement/mainBookManagement/mainBookManagement/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookManagement;
+namespace mainBookManagement
+{
+    class Program
+    {
+        public static int menu()
+        {
+            Console.WriteLine("\n******Book Management******");
+            Console.WriteLine("1. Add Book");
+            Console.WriteLine("2. Find book");
+            Console.WriteLine("3. Show Book");
+            Console.WriteLine("4. Remove Book");
+            Console.WriteLine("5. Exit Book");
+            int choice = 0;
+            Console.Write("Input Command: ");
+            choice = int.Parse(Console.ReadLine());
+            return choice;
+        }
+        public static void Main(string[] args)
+        {
+            int choice;
+            BookLib book = new BookLib();
+            do
+            {
+                choice = menu();
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            String move;
+                            do
+                            {
+                                book.GetBook();
+                                Console.WriteLine("\nDo you want to continue? (Yes/No)");
+                                move = Console.ReadLine();
+                            } while (move.Equals("Yes"));
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            String move;
+                            do
+                            {
+                                book.SearchBook();
+                                Console.WriteLine("\nDo you want to continue? (Yes/No)");
+                                move = Console.ReadLine();
+                            } while (move.Equals("Yes"));
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            String move;
+                            do
+                            {
+                                book.Show();
+                                Console.WriteLine("\nDo you want to continue? (Yes/No)");
+                                move = Console.ReadLine();
+                            } while (move.Equals("Yes"));
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            String move;
+                            do
+                            {
+                                book.RemoveBook();
+                                Console.WriteLine("\nDo you want to continue? (Yes/No)");
+                                move = Console.ReadLine();
+                            } while (move.Equals("Yes"));
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            Console.WriteLine("Thank you !!!");
+                            break;
+                        }
+                }
+                if (choice > 5) {
+                    Console.WriteLine("Please input command from 1 to 5 !!!");
+                }
+            } while (choice >= 1 && choice <= 4);
+            
+            
+        }
+    }
+}
